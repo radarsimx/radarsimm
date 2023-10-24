6f993ee25e49a475d4d4f002307244f2a93442c5
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2023-10-24 12:26:01 -0400

    Update RadarSim.m

--- @RadarSim/RadarSim.m
+++ @RadarSim/RadarSim.m
@@ -1,9 +1,22 @@
+% ----------
+% RadarSimM - Radar Simulator for MATLAB
+% Copyright (C) 2023 - PRESENT  radarsimx.com
+% E-mail: info@radarsimx.com
+% Website: https://radarsimx.com
+%
+%  ____           _            ____  _          __  __
+% |  _ \ __ _  __| | __ _ _ __/ ___|(_)_ __ ___ \ \/ /
+% | |_) / _` |/ _` |/ _` | '__\___ \| | '_ ` _ \ \  /
+% |  _ < (_| | (_| | (_| | |   ___) | | | | | | |/  \
+% |_| \_\__,_|\__,_|\__,_|_|  |____/|_|_| |_| |_/_/\_\
+
 classdef RadarSim < handle
     properties (Access = public)
         version_ = '1.1';
 
         samples_;
         pulses_;
+
         baseband_;
         timestamp_;
 

b39c348d19cd4ef26d16e2f1507108eedc1310ea
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-07-16 11:03:48 -0400

    revise for new version

--- examples/example_fmcw_radar.m
+++ examples/example_fmcw_radar.m
@@ -11,9 +11,23 @@
 
 clear;
 
-%% Create RadarSim handle
+%% Add path of the module
 
-rsim_obj=RadarSim;
+addpath("../src");
+
+%% Transmitter channel
+
+az_angle = -80:1:80;
+az_pattern = 20 * log10(cos(az_angle / 180 * pi).^4) + 6;
+
+el_angle = -80:1:80;
+el_pattern = 20 * log10((cos(el_angle / 180 * pi)).^20) + 6;
+
+tx_ch = RadarSim.TxChannel([0 0 0], ...
+    'azimuth_angle', az_angle, ...
+    'azimuth_pattern', az_pattern, ...
+    'elevation_angle', el_angle, ...
+    'elevation_pattern', el_pattern);
 
 %% Transmitter
 
@@ -24,17 +38,11 @@ fc = sum(f)/2;
 prp = 100e-6;
 num_pulses = 256;
 
-rsim_obj.init_transmitter(f, t, 'tx_power',10, 'prp', prp, 'pulses',num_pulses);
+tx=RadarSim.Transmitter(f, t, 'tx_power',10, 'prp', prp, 'pulses',num_pulses, 'channels',{tx_ch});
 
-%% Transmitter channel
+%% Receiver channel
 
-az_angle = -80:1:80;
-az_pattern = 20 * log10(cos(az_angle / 180 * pi).^4) + 6;
-
-el_angle = -80:1:80;
-el_pattern = 20 * log10((cos(el_angle / 180 * pi)).^20) + 6;
-
-rsim_obj.add_txchannel([0 0 0], ...
+rx_ch = RadarSim.RxChannel([0 0 0], ...
     'azimuth_angle', az_angle, ...
     'azimuth_pattern', az_pattern, ...
     'elevation_angle', el_angle, ...
@@ -47,27 +55,26 @@ noise_figure=12;
 rf_gain=20;
 resistor=500;
 bb_gain=30;
-rsim_obj.init_receiver(fs, rf_gain, resistor, bb_gain, 'noise_figure', noise_figure);
+rx = RadarSim.Receiver(fs, rf_gain, resistor, bb_gain, 'noise_figure', noise_figure, 'channels',{rx_ch});
 
-%% Receiver channel
+%% Radar
 
-rsim_obj.add_rxchannel([0 0 0], ...
-    'azimuth_angle', az_angle, ...
-    'azimuth_pattern', az_pattern, ...
-    'elevation_angle', el_angle, ...
-    'elevation_pattern', el_pattern);
+radar = RadarSim.Radar(tx, rx);
 
 %% Targets
 
-rsim_obj.add_point_target([200 0 0], [-5 0 0], 20, 0);
-rsim_obj.add_point_target([95 20 0], [-50 0 0], 15, 0);
-rsim_obj.add_point_target([30 -5 0], [-22 0 0], 5, 0);
+targets={};
+targets{1} = RadarSim.PointTarget([200 0 0], [-5 0 0], 20);
+targets{2} = RadarSim.PointTarget([95 20 0], [-50 0 0], 15);
+targets{3} = RadarSim.PointTarget([30 -5 0], [-22 0 0], 5);
 
 %% Run Simulation
 
-rsim_obj.run_simulator('noise', true);
-baseband=rsim_obj.baseband_;
-timestamp=rsim_obj.timestamp_;
+simc = RadarSim.Simulator();
+simc.Run(radar, targets, 'noise', true);
+
+baseband=simc.baseband_;
+timestamp=simc.timestamp_;
 
 figure();
 plot(timestamp(:,1,1), real(baseband(:,1,1)), 'LineWidth',1.5);
@@ -88,7 +95,7 @@ range_profile=fft(baseband.*repmat(chebwin(160,60),1,256), [], 1);
 max_range = (3e8 * fs * t / bw / 2);
 
 figure();
-surf(0:(num_pulses-1), linspace(0, max_range, rsim_obj.samples_), 20*log10(abs(range_profile(:,:,1))));
+surf(0:(num_pulses-1), linspace(0, max_range, radar.samples_per_pulse_), 20*log10(abs(range_profile(:,:,1))));
 shading interp;
 title('Range Profile');
 xlabel('Chirp');
@@ -105,7 +112,7 @@ rdop = fft(range_profile.*repmat(chebwin(256,60).',160,1), [], 2);
 unambiguous_speed = 3e8/prp/fc/2;
 
 figure();
-surf(linspace(-unambiguous_speed, 0, num_pulses), linspace(0, max_range, rsim_obj.samples_), 20*log10(abs(rdop(:,:,1))));
+surf(linspace(-unambiguous_speed, 0, num_pulses), linspace(0, max_range, radar.samples_per_pulse_), 20*log10(abs(rdop(:,:,1))));
 shading interp;
 title('Range Doppler');
 xlabel('Velocity (m/s)');
